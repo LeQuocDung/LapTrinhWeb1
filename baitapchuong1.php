9c939380343f23eaef449c2e90de7949f47cfbb5
LeQuocDung <Quocdungc08@gmail.com>
2018-03-28 22:19:46 +0700

    tuan 1
    lỡ xóa mất nên em nộp bài lại

---
+++ baitapchuong1.php
@@ -0,0 +1,60 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//bai 1
+echo 'Bài 1','<br>';
+echo "Tomorrow I'll learn PHP global variables <br>";
+//bai 2
+echo 'Bài 2 <br>';
+echo "Twinkle, Twinkle star star";
+//bai3
+echo 'Bài 3 <br>';
+$x=10;
+$y=7;
+echo '10 + 7 = '.$x+$y.'<br>';
+echo '10 - 7 = '.$x-$y.'<br>';
+echo '10*7 = '.$x*$y.'<br>';
+echo '10/7 = '.$x/$y.'<br>';
+echo '10%7 = '.$x%$y.'<br>';
+//bai 4
+echo 'Bài 4<br>';
+
+$x=10;
+$y=7;
+echo '10 + 7 = '.$x+$y.'<br>';
+echo '10 - 7 = '.$x-$y.'<br>';
+echo '10*7 = '.$x*$y.'<br>';
+echo '10/7 = '.$x/$y.'<br>';
+echo '10%7 = '.$x%$y.'<br>';
+echo 'Bài 6<br>';
+$a=8;
+echo $a=$a+2,"<br>";
+echo $a=$a-4,"<br>";
+echo $a=$a*5,"<br>";
+echo $a=$a/3,"<br>";
+echo $a=$a+1,"<br><br><br>";
+// bai 7
+echo 'Bài 8<br>';
+$thang = Date('F',time());
+echo $thang,"<br>";
+if($thang = 8);
+{
+    echo "đó là tháng 8, vì vậy thật nóng<br>";
+}
+if($thang =! 8);
+{
+echo"không phải tháng tám, vì vậy ít nhất không phải ở đỉnh điểm của nhiệt";
+}
+echo 'Bài 8<br>';
+$i = 'abc abc abc abc abc abc abc abc abc<br>';
+$j ='xyz xyz xyz xyz xyz xyz xyz xyz xyz<br>';
+$k = '1 2 3 4 5 6 7 8 9<br>';
+$m ='* Item 1<br> * Item 2<br> * Item 3<br> * Item 4<br> * Item 5<br>';
+echo $i;
+echo $j;
+echo $k;
+echo $m;
