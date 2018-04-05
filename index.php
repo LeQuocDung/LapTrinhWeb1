b85883cbb53d6e72250866f280e49de55a61a6fa
LeQuocDung <Quocdungc08@gmail.com>
2018-04-05 21:35:49 +0700

    bai tap tuan6
    bai tap tuan6

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <title></title>
+    <meta charset="utf-8">
+    <body>
+        <form action="login.php" method="post">
+            <input type="submit" name="nhap" value="dang nhap">
+        </form>
+        <h3>xin chao cac ban</h3>
+    </body>
+    
+</html>
+
